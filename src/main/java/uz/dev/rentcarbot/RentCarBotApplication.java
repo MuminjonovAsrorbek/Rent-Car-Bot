784034b5ad4dfacd24ec8efa1d9eba965007a919
Asrorbek <muminjonovasrorbek2004@gmail.com>
2025-08-05 14:21:31 +0500

    Create Project

---
+++ src/main/java/uz/dev/rentcarbot/RentCarBotApplication.java
@@ -0,0 +1,13 @@
+package uz.dev.rentcarbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentCarBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentCarBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/dev/rentcarbot/RentCarBotApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.dev.rentcarbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentCarBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
