e55bdb32ca12439871e0d34a5544495c6c21ba43
Asrorbek <muminjonovasrorbek2004@gmail.com>
2025-08-05 22:53:18 +0500

    fixed bug

--- src/main/java/uz/dev/rentcarbot/RentCarBotApplication.java
+++ src/main/java/uz/dev/rentcarbot/RentCarBotApplication.java
@@ -2,10 +2,10 @@ package uz.dev.rentcarbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableFeignClients
 public class RentCarBotApplication {
 
     public static void main(String[] args) {
