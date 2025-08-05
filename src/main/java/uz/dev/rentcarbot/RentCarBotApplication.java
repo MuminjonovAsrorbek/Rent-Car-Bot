b26d2a19aef1daee49d61e7496091544d22caa4f
Asrorbek <muminjonovasrorbek2004@gmail.com>
2025-08-05 21:25:13 +0500

    bug qismi bor

--- src/main/java/uz/dev/rentcarbot/RentCarBotApplication.java
+++ src/main/java/uz/dev/rentcarbot/RentCarBotApplication.java
@@ -3,11 +3,9 @@ package uz.dev.rentcarbot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
 public class RentCarBotApplication {
 
     public static void main(String[] args) {
